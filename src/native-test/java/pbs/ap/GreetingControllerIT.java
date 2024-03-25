6a32c94010c11bfba153f87202647691ac7174f7
Mikołaj Barański <mikbar002@o365.student.pbs.edu.pl>
2024-03-25 09:44:38 +0100

    Generate Quark stuff (again)

---
+++ src/native-test/java/pbs/ap/GreetingControllerIT.java
@@ -0,0 +1,8 @@
+package pbs.ap;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingControllerIT extends GreetingControllerTest {
+    // Execute the same tests but in packaged mode.
+}
