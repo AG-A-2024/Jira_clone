ea9312620f013473a75f300cee4ff09b08212d7d
So0ly <m.baranski2000@o2.pl>
2024-04-15 15:11:06 +0200

    Add project service

--- src/main/java/pbs/ap/projects/ProjectServiceImpl.java
+++ src/main/java/pbs/ap/projects/ProjectServiceImpl.java
@@ -1,11 +1,12 @@
 package pbs.ap.projects;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.jboss.logging.Logger;
 
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 @RequiredArgsConstructor
@@ -14,18 +15,32 @@ public class ProjectServiceImpl implements ProjectService{
     private static final Logger LOG = Logger.getLogger(ProjectServiceImpl.class);
     public List<Project> getAllProjects() {
         LOG.debug(">>>getAllProjects<<<");
-        return PanacheEntityBase.listAll();
+        return Project.listAll();
     }
 
     public List<Project> getAllUserProjects() {
         return null;
     }
 
-    public Project getProjectById(Long id) {
-        return null;
+    public Optional<Project> getProjectById(Long id) {
+        LOG.debug(">>>getProjectById<<<");
+        return Project.findByIdOptional(id);
     }
 
-    public Project getProjectByProjectName(String projectName) {
-        return null;
+    public Optional<Project> getProjectByProjectName(String projectName) {
+        LOG.debug(">>>getProjectByProjectName<<<");
+        return Project.find("projectName", projectName).firstResultOptional();
+    }
+
+    @Transactional
+    public boolean addProject(Project project){
+        LOG.debug(">>>addProject " + project.toString() + "<<<");
+        project.persistAndFlush();
+        return project.isPersistent();
+    }
+
+    @Transactional
+    public boolean deleteProjectById(Long id){
+        return Project.deleteById(id);
     }
 }
