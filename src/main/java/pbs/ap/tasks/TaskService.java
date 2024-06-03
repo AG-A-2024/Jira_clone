c8994d471efde58227221a1f575f3e858254f02a
chrisitstyle <infernos1244@o2.pl>
2024-06-03 15:18:41 +0200

    Add new http status for Tasks

--- src/main/java/pbs/ap/tasks/TaskService.java
+++ src/main/java/pbs/ap/tasks/TaskService.java
@@ -1,16 +1,16 @@
 package pbs.ap.tasks;
 
 
-import io.smallrye.mutiny.Uni;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskService {
 
     List<Task> getAllTasks();
     Task getTaskById(Long id);
    Task getTaskByTaskName(String taskName);
-   Uni<Task> addTask(Task task);
-    Task update(Task task);
+   Task addTask(Task task);
+    Task update(Long id, Task task);
     Task deleteTaskById(Long id);
+    Optional<Task> findTaskById(Long id);
 }
